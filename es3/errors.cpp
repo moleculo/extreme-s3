9302973d071aca5fa8e0b031c5b3963e7ca89c55
Alex Besogonov <Alex.Besogonov@gmail.com>
2012-01-03 01:48:12 +0200

    Backtrace hack

--- es3/errors.cpp
+++ es3/errors.cpp
@@ -11,6 +11,7 @@ extern ES3LIB_PUBLIC const result_code_t es3::sok=result_code_t();
 es3_exception::es3_exception(const result_code_t &code) : code_(code)
 {
 	std::stringstream s;
+	backtrace_it();
 	s<<("Error code: ")<<code.code()<<", description: "<<code.desc();
 	what_=s.str();
 }
