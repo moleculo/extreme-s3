09a1d930b8d819512494ecdb19b12119e3ae8c62
Alex Besogonov <Alex.Besogonov@gmail.com>
2012-01-03 12:03:09 +0200

    Checking

--- es3/connection.cpp
+++ es3/connection.cpp
@@ -435,7 +435,7 @@ std::string s3_connection::upload_data(const std::string &path,
 	time_t start=time(NULL);
 	curl_easy_perform(curl_);// | die;
 	if (result.find("<Error>")!=std::string::npos)
-		err(errWarn) << "!!!!! failed: "<< (data.last_read-start);
+		err(errWarn) << ("!!!!! failed: "+int_to_string(data.last_read-start)+"==");
 	//std::cerr << "Upload data " << result << std::endl;
 
 	if (!etag.empty() &&
