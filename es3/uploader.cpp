f2a262e7519cf904d295815c5923defa5f48e9c5
Alex Besogonov <Alex.Besogonov@gmail.com>
2012-01-04 05:00:18 +0200

    Extreme S3

--- es3/uploader.cpp
+++ es3/uploader.cpp
@@ -263,8 +263,8 @@ void file_uploader::start_upload(agenda_ptr ag,
 								 bool compressed)
 {
 	uint64_t size = 0;
-	for(auto iter=files->sizes_.begin();iter!=files->sizes_.end();++iter)
-		size+=*iter;
+	for(int f=0;f<files->sizes_.size();++f)
+		size+=files->sizes_.at(f);
 
 	size_t number_of_segments= size/conn_->segment_size_ +
 			((size%conn_->segment_size_)==0 ? 0:1);
